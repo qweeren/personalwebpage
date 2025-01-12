f228797e937c947c45c39ff2c77d7ee7b7cf3248
Eren <erenyordem2006@gmail.com>
2025-01-12 04:43:20 +0300

    build fix

--- qwerendev/vite.config.ts
+++ qwerendev/vite.config.ts
@@ -1,19 +1,6 @@
-import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    // Use the Vercel adapter for deployment
-    adapter: adapter(),
-    // You can customize paths, prerendering, etc., here if needed
-    alias: {
-      $lib: 'src/lib',
-      $routes: 'src/routes'
-    }
-  },
-  preprocess: vitePreprocess()
-};
-
-export default config;
+export default defineConfig({
+  plugins: [sveltekit()]
+});

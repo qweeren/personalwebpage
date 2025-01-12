f8aabeeb3bc8a6cc540293bb46d4a18b6dfce692
Eren <erenyordem2006@gmail.com>
2025-01-12 04:46:52 +0300

    build fix

--- qwerendev/vite.config.ts
+++ qwerendev/vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  
 });
